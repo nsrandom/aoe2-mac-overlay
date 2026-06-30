118c621cc362c47a8d4ddffb3ecaf5ec06457f44
Asif <github@nsrandom.com>
2026-06-29 22:33:26 -0700

    Initial Commit

---
+++ AoE Overlay/AoE_OverlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  AoE_OverlayApp.swift
+//  AoE Overlay
+//
+//  Created by Asif Sheikh on 6/29/26.
+//
+
+import SwiftUI
+
+@main
+struct AoE_OverlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
